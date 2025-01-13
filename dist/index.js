72fb3631e8290ca48548baef6f509c2e25d1e6b2
GMarqz <gabrielmarques9782@gmail.com>
2025-01-13 00:07:08 -0300

    compile

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,12 @@
 import renderTYPage from "./pages/thankYouPage.js";
 const $form = document.getElementById("form");
+const $submitBtn = document.getElementById("submit-btn");
+const $fieldset = document.getElementById("fieldset");
 $form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("Listening");
-    renderTYPage($form, 4);
+    const $selectedValue = document.querySelector('input[type="radio"]:checked');
+    if ($selectedValue) {
+        console.log(`You selected ${$selectedValue.value}`);
+    }
+    renderTYPage($form, $selectedValue.value);
 });
